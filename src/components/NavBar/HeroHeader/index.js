8db6b806b439e2f3ceb15cc6002106552f8ff733
masiaf <masiafrest@gmail.com>
2021-10-30 21:29:44 -0500

    add heroHeader and navbar

---
+++ src/components/NavBar/HeroHeader/index.js
@@ -0,0 +1,10 @@
+import './heroHeader.css'
+
+export default function HeroHeader() {
+  return (
+    <header class="hero" id="hero">
+      <h1>Julio Zeng</h1>
+      <p>HTML • CSS • Javascript • React • Nodejs</p>
+    </header>
+  )
+}
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,13 @@
+
+export default function NavBar() {
+  return (
+    <nav id="menu">
+      <a href="#1">Sección 1</a>
+      <a href="#2">Sección 2</a>
+      <a href="#3">Sección 3</a>
+      <a href="#4">Sección 4</a>
+      <a href="#5">Sección 5</a>
+      <span class="indicador" id="indicador"></span>
+    </nav>
+  )
+}
