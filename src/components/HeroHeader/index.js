0be69a19bf88458937e4307aa8e315f5eb1feeb4
masiaf <masiafrest@gmail.com>
2021-10-31 09:57:13 -0500

    change class to className

--- src/components/HeroHeader/index.js
+++ src/components/HeroHeader/index.js
@@ -2,7 +2,7 @@ import './HeroHeader.css'
 
 export default function HeroHeader() {
   return (
-    <header class="hero" id="hero">
+    <header className="hero" id="hero">
       <h1>Julio Zeng</h1>
       <p>HTML • CSS • Javascript • React • Nodejs</p>
     </header>
