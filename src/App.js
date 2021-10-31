491caee19adb0bebe6c0193b95144db281ec64b6
masiaf <masiafrest@gmail.com>
2021-10-31 16:37:17 -0500

    add mock section data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import NavBar from './components/NavBar';
-import HeroHeader from './components/HeroHeader';
-import Sections from './components/Sections';
+import NavBar from "./components/NavBar";
+import HeroHeader from "./components/HeroHeader";
+import Sections from "./components/Sections";
+
+const sectionData = [
+  {
+    title: "Seccion 1",
+    github: "github",
+    link: "link",
+  },
+];
+
+const mockSectionData = Array(5).map((e, i) => ({
+  title: `Seccion ${i}`,
+  github: "github",
+  link: "link",
+}));
 
 function App() {
   return (
