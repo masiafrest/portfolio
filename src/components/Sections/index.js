e0a29ba37bcd5df91e68730202b7e2bee3ea5e43
masiaf <masiafrest@gmail.com>
2021-10-31 16:21:13 -0500

    add css things

--- src/components/Sections/index.js
+++ src/components/Sections/index.js
@@ -1,47 +1,87 @@
-import './Sections.css'
+import "./Sections.css";
 
 export default function Sections() {
   return (
-    <section className="secciones">
-      <div className="seccion" id="1">
+    <section>
+      <div className="section" id="1">
         <div className="card">
           <h1>Sección 1</h1>
           <img src="img/1.jpg" alt="" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus congue sapien.Maecenas lacinia tellus nec turpis tempor pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque feugiat dolor quis sapien rutrum, sed placerat est volutpat.Curabitur sed convallis lacus, vel molestie ante.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit
+            amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus
+            quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus
+            congue sapien.Maecenas lacinia tellus nec turpis tempor
+            pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque
+            feugiat dolor quis sapien rutrum, sed placerat est
+            volutpat.Curabitur sed convallis lacus, vel molestie ante.
+          </p>
         </div>
       </div>
 
-      <div className="seccion" id="2">
+      <div className="section" id="2">
         <div className="card">
           <h1>Sección 2</h1>
           <img src="img/2.jpg" alt="" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus congue sapien.Maecenas lacinia tellus nec turpis tempor pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque feugiat dolor quis sapien rutrum, sed placerat est volutpat.Curabitur sed convallis lacus, vel molestie ante.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit
+            amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus
+            quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus
+            congue sapien.Maecenas lacinia tellus nec turpis tempor
+            pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque
+            feugiat dolor quis sapien rutrum, sed placerat est
+            volutpat.Curabitur sed convallis lacus, vel molestie ante.
+          </p>
         </div>
       </div>
 
-      <div className="seccion" id="3">
+      <div className="section" id="3">
         <div className="card">
           <h1>Sección 3</h1>
           <img src="img/3.jpg" alt="" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus congue sapien.Maecenas lacinia tellus nec turpis tempor pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque feugiat dolor quis sapien rutrum, sed placerat est volutpat.Curabitur sed convallis lacus, vel molestie ante.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit
+            amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus
+            quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus
+            congue sapien.Maecenas lacinia tellus nec turpis tempor
+            pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque
+            feugiat dolor quis sapien rutrum, sed placerat est
+            volutpat.Curabitur sed convallis lacus, vel molestie ante.
+          </p>
         </div>
       </div>
 
-      <div className="seccion" id="4">
+      <div className="section" id="4">
         <div className="card">
           <h1>Sección 4</h1>
           <img src="img/4.jpg" alt="" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus congue sapien.Maecenas lacinia tellus nec turpis tempor pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque feugiat dolor quis sapien rutrum, sed placerat est volutpat.Curabitur sed convallis lacus, vel molestie ante.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit
+            amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus
+            quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus
+            congue sapien.Maecenas lacinia tellus nec turpis tempor
+            pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque
+            feugiat dolor quis sapien rutrum, sed placerat est
+            volutpat.Curabitur sed convallis lacus, vel molestie ante.
+          </p>
         </div>
       </div>
 
-      <div className="seccion" id="5">
+      <div className="section" id="5">
         <div className="card">
           <h1>Sección 5</h1>
           <img src="img/5.jpg" alt="" />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus congue sapien.Maecenas lacinia tellus nec turpis tempor pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque feugiat dolor quis sapien rutrum, sed placerat est volutpat.Curabitur sed convallis lacus, vel molestie ante.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam sit
+            amet ante mattis, lacinia arcu nec, consequat massa.Aliquam faucibus
+            quam eget justo posuere vehicula.Donec ac fermentum dolor, rhoncus
+            congue sapien.Maecenas lacinia tellus nec turpis tempor
+            pellentesque.Praesent in erat a nisl viverra dapibus.Pellentesque
+            feugiat dolor quis sapien rutrum, sed placerat est
+            volutpat.Curabitur sed convallis lacus, vel molestie ante.
+          </p>
         </div>
       </div>
     </section>
-  )
+  );
 }
