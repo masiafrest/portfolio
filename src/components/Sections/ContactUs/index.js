d9e1cbd1bd129ccb2627ea12201a9f7eec517434
masiaf <masiafrest@gmail.com>
2021-11-05 16:28:04 -0500

    add form

--- src/components/Sections/ContactUs/index.js
+++ src/components/Sections/ContactUs/index.js
@@ -1,12 +1,11 @@
 import "./ContactUs.css";
+import Form from "./Form";
 
 export default function ContactUs({ data }) {
   return (
     <section>
       <h2>Contact Us</h2>
-      <form>
-        <label></label>
-      </form>
+      <Form />
     </section>
   );
 }
