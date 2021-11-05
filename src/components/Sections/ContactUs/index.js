1afbb779f67031d56f6971ce561df2222dd1a6f4
masiaf <masiafrest@gmail.com>
2021-11-05 16:31:08 -0500

    clean up git msg

--- src/components/Sections/ContactUs/index.js
+++ src/components/Sections/ContactUs/index.js
@@ -3,15 +3,9 @@ import Form from "./Form";
 
 export default function ContactUs({ data }) {
   return (
-<<<<<<< HEAD
-    <section>
-      <h2>Contact Us</h2>
-      <Form />
-=======
     <section className="contactUs">
       <h2>Contactame</h2>
-      <span>Email: masiafrest@gmail.com</span>
->>>>>>> cbf3dca18a17412fe21bf63e14332ca959e9038a
+      <Form />
     </section>
   );
 }
