6ff22176ac3c44317858a0fa1d63e3c982a9dfd5
Jzz <29693812+masiafrest@users.noreply.github.com>
2023-12-06 13:55:33 -0500

    add more lang

--- src/components/HeroHeader/index.js
+++ src/components/HeroHeader/index.js
@@ -1,10 +1,15 @@
 import "./HeroHeader.css";
 
+const languages = ["Rust", "HTML", "CSS", "Javascript","Typescript", "Python"]
+const libray = ["React", "Nodejs", "Material UI", "Django", "Express"]
+
 export default function HeroHeader({ setRef }) {
   return (
     <header ref={setRef} className="hero" id="hero">
       <h1>Julio Zeng</h1>
-      <p>HTML • CSS • Javascript • React • Nodejs</p>
+
+      <p>{languages.join(" • ")}</p>
+      <p>{libray.join(" • ")}</p>
     </header>
   );
 }
